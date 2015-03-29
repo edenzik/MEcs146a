af7ef7d4abd9c786c4a6e55afdf7764656e7b6ce
Michael Partridge <partridgems@gmail.com>
2015-03-29 01:10:22 -0400

    Struggling

--- src/channelTest.rs
+++ src/channelTest.rs
@@ -2,36 +2,49 @@ use std::sync::mpsc::{Sender, Receiver, channel};
 use std::sync::mpsc;
 use std::thread;
 
-struct Mine {
-    value: i32
-}
-
-static NUMTHREADS :i32 = 3;
+static NUMTHREADS :usize = 3;
 
 fn main() {
 
-    let mut chnl: (Sender, Receiver);
-    let mut counter :i32 = 0;
-    let mut in_pipe = Option::<Sender>;
+    // Maybe consider a Vec of channels instead?
+    // Initialize them all at once and pass in only the required
+    // references to threads as they are spawned.
 
+    let mut channels = Vec::new();
+
+    // Initialize a Vec full of channels
+    for _ in 0..(NUMTHREADS - 1) {
+        channels.push(channel::<String>());
+    }
+
+    // Initialize each thread, connect together with channels
     for id in 0..NUMTHREADS {
 
-        chnl = channel::<String>();
-        let thread_rx = pipe;
-        let thread_tx = chnl.0.clone();
-        let for_next_round = chnl.0.clone();
+        let thread_rx = match id {
+            0               => None,
+            n               => Some(&channels[n-1].1),
+        };
+        let thread_tx = match id {
+            x if x == NUMTHREADS-1  => None,
+            _                       => Some(&channels[id].0),
+        };
 
         thread::spawn(move || {
 
-            let prev_msg = thread_rx.recv();
+            let prev_msg = match thread_rx {
+                Some(sender)    => sender.recv().ok()
+                                    .expect("Could not read from channel").as_slice(),
+                None            => "**",
+            };
 
-            thread_tx.send(format!("Thread {} {}", id, prev_msg));
+            match thread_tx {
+                Some(receiver)  => {receiver.send(
+                    format!("Thread {} {}", id, prev_msg));}
+                None            => {println!("{} \n all done!", prev_msg);}
+            };
 
         });
 
-
-
-        in_pipe = for_next_round;
     }
 
 }
