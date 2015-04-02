d2021a2ff9e880450352afbd745a3d50c688c9fc
Michael Partridge <partridgems@gmail.com>
2015-04-02 15:53:26 -0400

    Testing implementing piping to/from processes

---
+++ src/commandPipeTest.rs
@@ -0,0 +1,57 @@
+extern crate getopts;
+
+
+// use getopts::{optopt, getopts};
+// use std::old_io::BufferedReader;
+use std::process;
+// use std::old_io::stdin;
+// use std::{old_io, os};
+use std::io::Read;
+use std::str;
+
+struct StdOutIter {
+    out: process::ChildStdout,
+}
+
+static test_string: &' static str = "This is \n a test \n string, yo.\n";
+
+impl<'a> Iterator for StdOutIter {
+    type Item = String;
+
+    fn next(& mut self) -> Option<String> {
+        let mut buffer_array : [u8; 50] = [0; 80];
+        let buffer = &mut buffer_array;
+        
+        let output_str = match self.out.read(buffer) {
+            Ok(length) => if length == 0 { return None }
+                            else { str::from_utf8(&buffer[0..length]) },
+            Err(_)   => { return None },
+        };
+
+        match output_str {
+            Ok(string) => Some(string.to_string()),
+            Err(_) => panic!("failed to convert stdin to String"),
+        }
+
+    }
+}
+
+fn main() {
+
+    let cmd = process::Command::new("wc")
+        .stdin(process::Stdio::capture())
+        .stdout(process::Stdio::capture())
+        .stderr(process::Stdio::capture()).spawn().unwrap();
+
+    let /*mut*/ stdout = cmd.stdout.unwrap();
+
+    let stdin_thread = move || {
+        let stdin = cmd.stdin.unwrap();
+    };
+
+    let process_reader = StdOutIter{ out : stdout };
+
+    for output in process_reader {
+        print!("{}", output);
+    }
+}
