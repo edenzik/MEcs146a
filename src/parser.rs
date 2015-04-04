9f753c30d019535853edd0ff4640085e6fe9769a
Eden Zik <edenzik@gmail.com>
2015-04-04 04:42:32 -0400

    some basic changes to parser, exprimenting. no comments yet - still using the provided code to figure out how to plump into new_main...

--- src/parser.rs
+++ src/parser.rs
@@ -1,15 +1,34 @@
-use std::mem::replace;
+extern crate getopts;
 
+
+use std::thread;
+use getopts::{optopt, getopts};
+use std::old_io::BufferedReader;
+use std::old_io::stdin;
+use std::{old_io, os};
+use std::str;
+use std::sync::mpsc;
+
+use std::error::Error;
+use std::io::prelude::*;
+use std::process::{Command, Stdio};
 fn main() {
+    let opt_cmd_line = get_cmdline_from_args();
+
+    match opt_cmd_line {
+        Some(cmd_line) => Shell::new("").run_cmdline(cmd_line.as_slice()),
+        None           => Shell::new("gash > ").run(),
+    }
+}
+
+
+fn old_main() {
     let input = "cat hello|grep moo";
     let x = GashCommandLine::new(input.as_slice());
     match x {
         GashCommandLine::Foreground(v) => {
             for a in v{
-                match a{
-                    GashCommand::Normal(l) => println!("{:?}", l.operator),
-                    _ => println!("")
-                }
+                a.spawn();
             }
         }
         _ => {}
@@ -20,19 +39,25 @@ fn main() {
 enum GashCommandLine<'a> {
     Foreground(Vec<GashCommand<'a>>),
     Background(Vec<GashCommand<'a>>),
-    Empty
+    Empty,
+    Exit
 }
 
 impl<'a> GashCommandLine<'a> {
     fn new(line : & 'a str) -> GashCommandLine<'a> {
+        if line.is_empty() {
+            return GashCommandLine::Empty;
+        }
+        if line.words().next().unwrap() == "exit" {
+            return GashCommandLine::Exit;
+        }
         let mut commands = Vec::new();
         for command_str in line.split('|'){
             commands.push(GashCommand::new(command_str));
         }
         match line.chars().last().unwrap(){
             '&' => GashCommandLine::Background(commands),
-            _ if !commands.is_empty() => GashCommandLine::Foreground(commands),
-            _ => GashCommandLine::Empty            
+            _   => GashCommandLine::Foreground(commands),           
         }
     }
 }
@@ -57,6 +82,10 @@ impl<'a> GashCommand<'a> {
         }
         GashCommand::BadCommand
     }
+
+    fn spawn(&self){
+        thread::scoped(move || {println!("this is thread number ");});
+    }
 }
 
 
@@ -76,4 +105,109 @@ enum GashCommand<'a> {
     OutputRedirect(GashOperation<'a>, Box<& 'a str>),
     BadCommand,
 }
+
+
+
+
+
+
+
+
+
+struct Shell<'a> {
+    cmd_prompt: &'a str,
+}
+
+impl <'a>Shell<'a> {
+    fn new(prompt_str: &'a str) -> Shell<'a> {
+        Shell { cmd_prompt: prompt_str }
+    }
+
+    fn run(&self) {
+        let mut stdin = BufferedReader::new(stdin());
+        let mut history: Vec<String> = Vec::new();
+        loop {
+            old_io::stdio::print(self.cmd_prompt.as_slice());
+            old_io::stdio::flush();
+            let line = stdin.read_line().unwrap();
+            let cmd_line = line.trim();
+            let command = GashCommandLine::new(cmd_line);
+            match command {
+                GashCommandLine::Foreground(cmds) => self.run_cmds(cmds),
+                GashCommandLine::Background(cmds) => println!("back task!"),
+                GashCommandLine::Exit             => return,
+                _                                 => println!("other")
+            }
+
+            history.push(String::from_str(cmd_line));
+        }
+    }
+
+    fn run_cmds(&self, cmds: Vec<GashCommand>){
+        for cmd in cmds {
+            match cmd {
+                GashCommand::Normal(op) => {
+                    let output = Command::new(*op.operator).args(&*op.operands.as_slice()).output().unwrap_or_else(|e| {panic!("failed to execute process: {}", e)});
+                    let stderr=String::from_utf8_lossy(&output.stderr);
+                    let stdout=String::from_utf8_lossy(&output.stdout);
+                    if !"".eq(stdout.as_slice()) {
+                        print!("{}", stdout);
+                    }
+                    if !"".eq(stderr.as_slice()) {
+                        print!("{}", stderr);
+                    }
+
+                },
+                _ => println!("shit happens")
+            };
+        }
+    }
+
+    fn run_cmdline(&self, cmd_line: &str) {
+        let argv: Vec<&str> = cmd_line.split(' ').filter_map(|x| {
+            if x == "" {
+                None
+            } else {
+                Some(x)
+            }
+        }).collect();
+
+        match argv.first() {
+            Some(&program) => self.run_cmd(program, argv.tail()),
+            None => (),
+        };
+    }
+
+    fn run_cmd(&self, program: &str, argv: &[&str]) {
+        if self.cmd_exists(program) {
+            let output = Command::new(program).args(argv).output().unwrap_or_else(|e| {panic!("failed to execute process: {}", e)});
+            let stderr=String::from_utf8_lossy(&output.stderr);
+            let stdout=String::from_utf8_lossy(&output.stdout);
+            if !"".eq(stdout.as_slice()) {
+                print!("{}", stdout);
+            }
+            if !"".eq(stderr.as_slice()) {
+                print!("{}", stderr);
+            }
+        } else {
+            println!("{}: command not found", program);
+        }
+    }
+
+    fn cmd_exists(&self, cmd_path: &str) -> bool {
+        Command::new("which").arg(cmd_path).stdout(Stdio::capture()).status().unwrap().success()
+    }
+}
+
+fn get_cmdline_from_args() -> Option<String> {
+    /* Begin processing program arguments and initiate the parameters. */
+    let args = os::args();
+
+    let opts = &[
+        getopts::optopt("c", "", "", "")
+        ];
+
+    getopts::getopts(args.tail(), opts).unwrap().opt_str("c")
+}
+
 
