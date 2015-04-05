df8830158b23fce02135d0f7466d7dabed192b64
Michael Partridge <partridgems@gmail.com>
2015-04-05 00:27:12 -0400

    file_io ready for integration

--- src/file_io_test.rs
+++ src/file_io_test.rs
@@ -3,7 +3,33 @@ use std::io::{self, Read, Write};
 use std::path::Path;
 use std::sync::mpsc::{channel, Sender, Receiver};
 use std::thread::{JoinHandle, spawn};
+use std::error;
 use std::str;
+
+
+fn main() {
+    // Create a path to the desired file
+    let path = Path::new("hello.txt");
+    let display = path.display();
+
+    // Open the path in read-only mode, returns `IoResult<File>`
+    let mut file = match File::open(&path) {
+        // The `desc` field of `IoError` is a string that describes the error
+        Err(why) => panic!("couldn't open {}: {}", display,
+                           error::Error::description(&why)),
+                           Ok(file) => file,
+    };
+
+    // Read the file contents into a string, returns `IoResult<String>`
+    let mut s = String::new();
+    match file.read_to_string(&mut s) {
+        Err(why) => panic!("couldn't read {}: {}", display,
+                           error::Error::description(&why)),
+                           Ok(_) => print!("{} contains:\n{}", display, s),
+    }
+
+    // `file` goes out of scope, and the "hello.txt" file gets closed
+}
 
 // This method to read from file and write to channel
 fn create_thread_io(channel : Sender<String>, file_name: Box<String>)
